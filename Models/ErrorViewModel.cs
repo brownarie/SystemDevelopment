1566782a48303497b4d06a935de4ce8c6e3b1834
RC_Student_lab <RC_Student_lab@labG9AEB3>
2024-09-09 17:25:49 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SystemDevelopment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/systemsdev.cs
@@ -0,0 +1,19 @@
+﻿namespace SystemDevelopment.Models
+{
+    public class SystemsDevelopModel
+    {
+
+        public string Name { get; set; }    
+        public string Surname { get; set; }
+        public string Description { get; set; }
+        public string IssueType { get; set; }
+        public string Address {  get; set; }     
+
+        public int LastPayment { get; set; }
+
+        public int LatestPayment { get; set; }
+
+     
+
+    }
+}
